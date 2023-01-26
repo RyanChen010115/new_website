c612303d9053c2bd39385290c241fef9d676ed7e
Ryan Chen <c_siyuan2001@hotmail.com>
2023-01-26 18:01:07 -0500

    navbar mostly done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-        <div className="Home">
+        <div className="gradient__bg">
             <Navbar />
             <Header />
         </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>
-      Navbar
+    <div className='navbar'>
+      <div className='navbar-links'>
+        <div className='navbar-links_container'>
+          <p><a href="home">Home</a></p>
+          <p><a href="intro">Introduction</a></p>
+          <p><a href="projects">Projects</a></p>
+          <p><a href="resume">Resume</a></p>
+        </div>
+      </div>
+      <div className='navbar-menu'></div>
     </div>
   )
 }
