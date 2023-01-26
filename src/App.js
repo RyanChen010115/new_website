176cd22f104465dbbe10e893e5f9e05224654058
Ryan Chen <c_siyuan2001@hotmail.com>
2023-01-25 23:02:00 -0500

    starting new website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Footer, Blog, Header, Features} from './containers';
+import { Article, Navbar, Intro } from './components';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Home">
+            <Navbar />
+            <Header />
+        </div>
+        <Intro />
+        <Features />
+        <Article />
+        <Blog />
+        <Footer />
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './intro.css';
+const Intro = () => {
+  return (
+    <div>
+      Intro
+    </div>
+  )
+}
+
+export default Intro
