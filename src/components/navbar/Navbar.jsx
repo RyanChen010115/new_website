13fbe2b3b397491b55bbaa65b1a6845c13fd8f29
Ryan Chen <c_siyuan2001@hotmail.com>
2023-01-30 18:51:24 -0500

    navbar changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,19 +2,36 @@ import React, { useState } from 'react';
 import './navbar.css';
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 
+
+const Menu = () =>(
+  <>
+  <p><a href="home">Home</a></p>
+  <p><a href="intro">Introduction</a></p>
+  <p><a href="projects">Projects</a></p>
+  <p><a href="resume">Resume</a></p>
+  </>
+)
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='navbar'>
       <div className='navbar-links'>
         <div className='navbar-links_container'>
-          <p><a href="home">Home</a></p>
-          <p><a href="intro">Introduction</a></p>
-          <p><a href="projects">Projects</a></p>
-          <p><a href="resume">Resume</a></p>
+          <Menu />
         </div>
       </div>
-      <div className='navbar-menu'></div>
+      <div className="navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>}
+        {toggleMenu && (
+          <div className="navbar-menu_container scale-up-center">
+            <div className='navbar-menu_container-links'>
+              <Menu />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
